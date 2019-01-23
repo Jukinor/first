7627c7de6c1cf758500551649aa69623bb70b4c1
Jukinor <ivbolgov@gmail.com>
2019-01-23 13:09:39 +0300

    MyProgram

---
+++ programms/first.py
@@ -0,0 +1,10 @@
+number1 = input("Введите A ")
+number2 = input("Введите B ")
+number1 = int(number1)
+number2 = int(number2)
+if number1>number2:
+	print("A больше B")
+else:
+	print("A < B")
+summa = number1 + number2				
+print("Summa=", summa)
